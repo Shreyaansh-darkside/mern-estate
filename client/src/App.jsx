34e83837d66f5a9bd4a9e901721b57f1a1b65899
Shree <shreyaanshjhs@gmail.com>
2023-10-31 16:17:51 +0530

    setup backend and frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import React from 'react'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import Header from './components/Header'
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/sign-in" element={<SignIn/>}/>
+        <Route path="/sign-up" element={<SignUp/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/about" element={<About/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
